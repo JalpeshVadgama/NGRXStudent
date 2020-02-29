eedbf50e6cc812d500e1df8b036529986b0c403e
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2020-03-01 00:55:47 +0530

    Added student and other routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  {
+    path: "students",
+    loadChildren: "../app/students/stuents.module#StudentModule"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/students/students.module.ts
+++ src/app/students/students.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { StudentComponent } from './student/student.component';
-import { StudentAddComponent } from './student-add/student-add.component';
-import { StudentEditComponent } from './student-edit/student-edit.component';
-import { StudentListComponent } from './student-list/student-list.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { StudentComponent } from "./student/student.component";
+import { StudentAddComponent } from "./student-add/student-add.component";
+import { StudentEditComponent } from "./student-edit/student-edit.component";
+import { StudentListComponent } from "./student-list/student-list.component";
+import { RouterModule, Routes } from "@angular/router";
 
-
-
+const studentRoutes: Routes = [{ path: "", component: StudentComponent }];
 @NgModule({
-  declarations: [StudentComponent, StudentAddComponent, StudentEditComponent, StudentListComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    StudentComponent,
+    StudentAddComponent,
+    StudentEditComponent,
+    StudentListComponent
+  ],
+  imports: [CommonModule, RouterModule.forChild(studentRoutes)]
 })
-export class StudentsModule { }
+export class StudentsModule {}
