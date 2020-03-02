86ebdda17c815d3605a2af6b6f9e4eb07cc74169
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2020-03-02 11:50:12 +0530

    Fixed student moduel lazy loading issue with import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
+import { StudentsModule } from "./students/students.module";
 import { HomeComponent } from "./home/home.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
@@ -11,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
